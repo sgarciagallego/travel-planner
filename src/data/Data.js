c146a226d39d29c46f470acfc690a3f5de5cf892
sgarciagallego <gallegogarciasergio@gmail.com>
2022-06-08 10:56:28 +0100

    initial commit - added data that I can work with

---
+++ src/data/Data.js
@@ -0,0 +1,40 @@
+const Data = [
+  {
+    id: 1,
+    title: 'Kigni Atki',
+    location: 'Preveza, Greece',
+    googleMapsLink: 'https://www.google.com/maps/place/Kiani+Akti/@38.9473717,20.7333345,14z/data=!4m13!1m7!3m6!1s0x135c38f87d22b779:0x312da7b3f30ddbaa!2sKiani+Akti!3b1!8m2!3d38.9473717!4d20.750844!3m4!1s0x135c38f87d22b779:0x312da7b3f30ddbaa!8m2!3d38.9473717!4d20.750844?hl=en',
+    date: {
+      start: '26 Jun, 2022',
+      end: '27 Jun 2022',
+    },
+    description: 'A beautiful beach located to the South of Preveza',
+    img: './images/kiani-akti.jpeg'
+  },
+  {
+    id: 2,
+    title: 'Luna Park',
+    location: 'Preveza, Greece',
+    googleMapsLink: 'https://www.google.com/maps/place/Luna+Park/@38.9628336,20.7554205,15z/data=!4m2!3m1!1s0x0:0x5569ab9094c30e7f?sa=X&hl=en&ved=2ahUKEwjr4P2Qyp34AhWmgVwKHW6MCOoQ_BJ6BAhnEAU',
+    date: {
+      start: '27th Jun, 2022',
+      end: '27th Jun, 2022',
+    },
+    description: 'A beautifully isolated Theme Park by the coast of Eastern Preveza',
+    img: './images/luna-park.jpeg'
+  },
+  {
+    id: 3,
+    title: 'Marina Preveza',
+    location: 'Preveza, Greece',
+    googleMapsLink: 'https://www.google.com/maps/place/Marina+Preveza/@38.9611485,20.7553075,17z/data=!3m1!4b1!4m5!3m4!1s0x135c39618ed22895:0x7fd6fb5fe4b80293!8m2!3d38.9611456!4d20.7575003',
+    date: {
+      start: '26th Jun, 2022',
+      end: '28th Jun, 2022',
+    },
+    description: 'The popular destination for yachts and boats, a beautiful fishing hub',
+    img: './images/marina-preveza.jpeg'
+  }
+]
+
+export default Data;
